9f108d1d04c0074539d472bb717b5427234e055d
User <meniodurao@gmail.com>
2024-06-25 01:30:59 +0200

    Calculando o imc

--- app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
+++ app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
@@ -24,9 +24,12 @@ class MainActivity : AppCompatActivity() {
         val btnCalcular = findViewById<Button>(R.id.btn_Calcular)
 
         btnCalcular.setOnClickListener {
-            val peso = edtPeso.text
-            val altura = edtAltura.text
-            println("Menio acao do botao  " + altura)
+            val peso : Float = edtPeso.text.toString().toFloat()
+            val altura : Float = edtAltura.text.toString().toFloat()
+
+            val alturaQ2 = altura * altura
+            val resultado = peso / alturaQ2
+            println("Menio acao do botao  " + resultado)
         }
 
 
